0a22ee2c4f2b5023258cb38176075e73f1291ff6
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2022-07-17 22:39:24 +0200

    Animate the spiral drawing

--- main.c
+++ main.c
@@ -4,17 +4,14 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define MAX_STEPS 580
+#define MAX_STEPS 500
 #define STEP_SIZE 25
-
-#define FONT_SIZE 30
 #define NODE_RADIUS 8
+#define NODE_COLOR {0xaa, 0xaa, 0xaa, 0xff}
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 800
-
 #define WINDOW_MID_X WINDOW_WIDTH/2
 #define WINDOW_MID_Y WINDOW_HEIGHT/2
-
 #define BLACK_COLOR {0x00, 0x00, 0x00, 0xff}
 
 SDL_Window* window;
@@ -98,6 +95,28 @@ bool is_prime(int maybe_prime) {
   return true;
 }
 
+SDL_Point interpolate(SDL_Point point, MoveDirection direction, int progress) {
+  switch (direction) {
+    case DIR_EAST:
+      point.x -= STEP_SIZE - progress;
+      return point;
+
+    case DIR_WEST:
+      point.x += STEP_SIZE - progress;
+      return point;
+
+    case DIR_NORTH:
+      point.y += STEP_SIZE - progress;
+      return point;
+
+    case DIR_SOUTH:
+      point.y -= STEP_SIZE - progress;
+      return point;
+  }
+}
+
+int segment_progress = 0;
+int draw_step = 2;
 void draw_ulam_spiral() {
   clear_screen();
 
@@ -105,12 +124,24 @@ void draw_ulam_spiral() {
   MoveDirection direction = DIR_EAST;
   int steps_per_turn = 1;
 
-  SDL_Color circle_color = {0x00, 0x0, 0x0, 0xff};
+  SDL_Color circle_color = NODE_COLOR;
 
-  for (int i = 1; i < MAX_STEPS; i++) {
+  for (int i = 1; i <= draw_step; i++) {
     SDL_Point new_point = calculate_next_point(last_point, direction);
+
+    if (i == draw_step && draw_step < MAX_STEPS) {
+      segment_progress+=5;
+      new_point = interpolate(new_point, direction, segment_progress);
+    }
+
+    if (segment_progress >= STEP_SIZE && draw_step < MAX_STEPS) {
+      draw_step++;
+      segment_progress = 0;
+    }
+
     thickLineRGBA(renderer, last_point.x, last_point.y, new_point.x, new_point.y, 2, 0x00, 0x00, 0x00, 0xff);
-    if(is_prime(i)) {
+
+    if (is_prime(i)) {
       aaFilledCircle(renderer, last_point.x, last_point.y, NODE_RADIUS, circle_color);
     }
 
@@ -129,7 +160,6 @@ void draw_ulam_spiral() {
 
 int main(int argc, char* argv[]) {
   SDL_Init(SDL_INIT_VIDEO);
-
   SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
   SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
